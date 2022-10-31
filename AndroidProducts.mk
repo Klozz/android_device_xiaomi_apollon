733bbb3e5fad6bb05228b4149291e7d305eaa573
klozz <carlosj@klozz.dev>
2022-11-01 00:28:34 +0100

    apollon: xperience bringup
    Signed-off-by: klozz <carlosj@klozz.dev>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_apollon.mk
+    $(LOCAL_DIR)/xperience_apollon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_apollon-user \
-    lineage_apollon-userdebug \
-    lineage_apollon-eng
+    xperience_apollon-user \
+    xperience_apollon-userdebug \
+    xperience_apollon-eng
